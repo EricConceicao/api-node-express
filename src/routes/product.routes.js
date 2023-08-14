3698272c1e7671cc49d60509ebc2fc8919ba6817
Eric Ribeiro <eric.r@aluno.ifsp.edu.br>
2023-08-14 17:26:23 -0300

    modification on product route

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 
 const router = express.Router();
 
@@ -11,8 +11,19 @@ router.get('/', (req, res) => {
     res.json({ PRODUCT });
 });
 
-router.post('/', (req, res) => {
-    res.json({ status: 'Recebendo' });
+router.post('/:query', (req, res) => {
+    const PRODUCT = [
+        { nome: "bola", qtd: 1, descricao:'Bola redonda de borracha' },
+        { nome: "copo", qtd: 3, descricao:'Copo da Coca' },
+        { nome: "garrafa", qtd: 7, descricao:'Garrafa de vidro' }
+    ];
+
+    // Filtra o vetor e retonar um objeto que tenha o nome correspondente
+    const product = PRODUCT.filter(query => {
+        return query.nome !== req.params.query
+    });
+
+    res.json({ product });
 });
 
 router.put('/', (req, res) => {
